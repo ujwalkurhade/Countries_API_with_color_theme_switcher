504c0c0208bfc21ca5e3f828443a285e2f44f435
Ujwal <ujwalkurhade05@gmail.com>
2023-06-16 10:24:23 +0530

    complit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CountryList from "./CountryList";
 import Header from "./Header";
+import axios from "axios";
 
 function Home() {
   const [value, setValue] = useState("");
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonblob.com/api/1119106135625056256")
+      .then((res) => setFilterData(res.data));
+  }, []);
+
+  const handelChange = (e) => {
+    setValue(e.target.value);
+    setFilterData(
+      filterData.filter(
+        (data) =>
+          data.name
+            .toLowerCase()
+            .includes(value.toLowerCase()) ||
+          data.region
+            .toLowerCase()
+            .includes(value.toLowerCase())
+      )
+    );
+    console.log(filterData);
+  };
+
   return (
     <div>
       {/* header */}
@@ -14,7 +39,8 @@ function Home() {
           <input
             placeholder="Search for a country..."
             value={value}
-            setValue={(e) => e.target.value}
+            on
+            onChange={handelChange}
           />
           <i className="bx bx-search "></i>
         </div>
@@ -31,7 +57,7 @@ function Home() {
       </div>
 
       {/* Countrys */}
-      <CountryList />
+      {<CountryList filterData={filterData} />}
     </div>
   );
 }
