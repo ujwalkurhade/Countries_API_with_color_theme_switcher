fea32283bae7ca34eafc7e324157c2bb457cc572
Ujwal <ujwalkurhade05@gmail.com>
2023-06-16 10:24:50 +0530

    hi

--- src/component/CountryList.jsx
+++ src/component/CountryList.jsx
@@ -1,26 +1,13 @@
 import React, { useEffect, useState } from "react";
-import data from "../data.json";
+
 import Card from "./Card";
 
-function CountryList({ value }) {
-  const [country, setcountry] = useState([]);
-  const [filterData, setFilterData] = useState([]);
-
-  useEffect(() => {
-    setcountry(data);
-  }, []);
-
-  setFilterData(
-    country.filter((data) =>
-      data.name.toLowerCase().includes(value.toLowerCase())
-    )
-  );
-
+function CountryList({ filterData, country }) {
   return (
     <div className="container">
       <h1>CountryList</h1>
       <div className="countryList">
-        {country.map((item, index) => {
+        {filterData.map((item, index) => {
           return (
             <Card
               key={`id is ${item.name}`}
